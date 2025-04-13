6fa087926426861a87575ca1aefc75b46a2dc696
TheElliotM <elliotmichlin@gmail.com>
2025-04-13 17:42:00 -0500

    changed motor minval to 750

--- esc_TEST.py
+++ esc_TEST.py
@@ -17,7 +17,7 @@ pi = pigpio.pi()
 pi.set_servo_pulsewidth(ESC, 0)
 
 max_value = 2000  # change this if your ESC's max value is different or leave it be
-min_value = 700  # change this if your ESC's min value is different or leave it be
+min_value = 750  # change this if your ESC's min value is different or leave it be
 print("For first time launch, select calibrate")
 print("Type the exact word for the function you want")
 print("calibrate OR manual OR control OR arm OR stop")
