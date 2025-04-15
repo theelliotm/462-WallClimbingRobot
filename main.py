bfe57dc30fd743130cef7941d095d8bcf60c4cd7
TheElliotM <elliotmichlin@gmail.com>
2025-04-15 17:47:43 -0500

    turning improvements

--- main.py
+++ main.py
@@ -10,12 +10,6 @@ os.system("sudo pigpiod")  # Launching GPIO library
 
 time.sleep(1)
 
-os.system("sudo systemctl enable bluetooth")
-
-# time.sleep(5)
-
-os.system("sudo systemctl start bluetooth")
-
 # time.sleep(5)
 
 MOTOR_LEFT_FORWARD_PIN = 23
@@ -59,6 +53,9 @@ print("Awaiting Command... Options: (c)ontroller, (t)ests")
 inp = input()
 if inp == 'c' or inp == 'controller':
     
+    os.system("sudo systemctl enable bluetooth")
+    os.system("sudo systemctl start bluetooth")
+    
     pygame.init()
     pygame.joystick.init()
 
@@ -72,7 +69,7 @@ if inp == 'c' or inp == 'controller':
     while True:
         pygame.event.pump()
         
-        #  0 - A, 1 - B, 2- X, 3 - Y, 4 - minus, 5 - home, 6 - plus, 
+        # BUTTON 0 - A, 1 - B, 2- X, 3 - Y, 4 - minus, 5 - home, 6 - plus, 
         # 7 - left stick, 8 - right stick, 9 - left shoulder, 10 - right shoulder
         # 11 - dpad up, 12 - dpad down, 13 - dpad left, 14 - dpad right
         
@@ -85,33 +82,38 @@ if inp == 'c' or inp == 'controller':
         #         buttons_pressed.remove(i)
         #         print(f"Button {i} released")
         
-        # 0 - Left X, 1 - Left Y (inverse), 2 - Right X, 3 - Right Y (inverse)
+        # AXIS 0 - Left X, 1 - Left Y (inverse), 2 - Right X, 3 - Right Y (inverse)
         # RANGE (-1, 1) 
+        
+        # AXIS 4 - Left Trigger, 5 - Right Trigger
+        # Either -1 or 0.9999...
         
         # for i in range(joystick.get_numaxes()):
         #     axis = joystick.get_axis(i)
         #     if abs(axis) > 0.1:
         #         print(f"Axis {i} value: {axis}")
+        
+        
+        
 else:
     while True:
-        print("Available Tests: (1) Drive Forward for X seconds (2) Drive backwards for X seconds (3) Enable prop motor for X seconds (4) Turn left one rotation (5) Turn right one rotation")
+        print("Available Tests: (1) Drive Forward for X seconds (2) Drive backwards for X seconds (3) Enable prop motor for X seconds (4) Turn left for X secs  (5) Turn right X secs")
         inp = input()
-        if inp == 1:
+        if int(inp) == 1:
             print("How many seconds?")
-            secs = input()
+            secs = float(input())
             print("Speed %? (0 - 100)")
-            speed = input()
+            speed = float(input())
 
-            if speed < 10:
-                speed = 10
-            elif speed > 100:
-                speed = 100
+            # convert speed (Scale 0 - 100) to scale 75 - 100. set speed to 0 if scale is 0
+            if speed != 0:
+                speed = ((speed / 100) * 30) + 70;
 
             pwm_left.start(speed)
             pwm_right.start(speed)
 
-            GPIO.output(MOTOR_LEFT_FORWARD_PIN, GPIO.HIGH)
-            GPIO.output(MOTOR_LEFT_BACKWARD_PIN, GPIO.LOW)
+            GPIO.output(MOTOR_LEFT_FORWARD_PIN, GPIO.LOW)
+            GPIO.output(MOTOR_LEFT_BACKWARD_PIN, GPIO.HIGH)
             GPIO.output(MOTOR_RIGHT_FORWARD_PIN, GPIO.HIGH)
             GPIO.output(MOTOR_RIGHT_BACKWARD_PIN, GPIO.LOW)
 
@@ -120,17 +122,21 @@ else:
             pwm_left.stop()
             pwm_right.stop()
 
-        elif inp == 2:
+        elif int(inp) == 2:
             print("How many seconds?")
-            secs = input()
+            secs = float(input())
             print("Speed %? (0 - 100)")
-            speed = input()
+            speed = float(input())
+            
+            # convert speed (Scale 0 - 100) to scale 75 - 100. set speed to 0 if scale is 0
+            if speed != 0:
+                speed = ((speed / 100) * 30) + 70;
 
             pwm_left.start(speed)
             pwm_right.start(speed)
 
-            GPIO.output(MOTOR_LEFT_FORWARD_PIN, GPIO.LOW)
-            GPIO.output(MOTOR_LEFT_BACKWARD_PIN, GPIO.HIGH)
+            GPIO.output(MOTOR_LEFT_FORWARD_PIN, GPIO.HIGH)
+            GPIO.output(MOTOR_LEFT_BACKWARD_PIN, GPIO.LOW)
             GPIO.output(MOTOR_RIGHT_FORWARD_PIN, GPIO.LOW)
             GPIO.output(MOTOR_RIGHT_BACKWARD_PIN, GPIO.HIGH)
 
@@ -139,11 +145,11 @@ else:
             pwm_left.stop()
             pwm_right.stop()
 
-        elif inp == 3:
+        elif int(inp) == 3:
             print("How many seconds?")
-            secs = input()
+            secs = float(input())
             print("Speed %? (0 - 100)")
-            speed = input()
+            speed = float(input())
 
             # pi.set_servo_pulsewidth(ESC_PIN, MIN_SPEED +
             #                         ((MAX_SPEED - MIN_SPEED) * (speed / 100)))
@@ -153,46 +159,48 @@ else:
             # pi.set_servo_pulsewidth(ESC_PIN, 0)
             # pi.stop()
 
-        elif inp == 4:
+        elif int(inp) == 4:
+            print("How many seconds?")
+            secs = float(input())
             print("Speed %? (0 - 100)")
-            speed = input()
+            speed = float(input())
 
-            if speed < 10:
-                speed = 10
-            elif speed > 100:
-                speed = 100
-
-            pwm_left.start(speed)
-            pwm_right.start(speed)
-
-            GPIO.output(MOTOR_LEFT_FORWARD_PIN, GPIO.LOW)
-            GPIO.output(MOTOR_LEFT_BACKWARD_PIN, GPIO.HIGH)
-            GPIO.output(MOTOR_RIGHT_FORWARD_PIN, GPIO.HIGH)
-            GPIO.output(MOTOR_RIGHT_BACKWARD_PIN, GPIO.LOW)
-
-            time.sleep(3)
-
-            pwm_left.stop()
-            pwm_right.stop()
-
-        elif inp == 5:
-            print("Speed %? (0 - 100)")
-            speed = input()
-
-            if speed < 10:
-                speed = 10
-            elif speed > 100:
-                speed = 100
+            # convert speed (Scale 0 - 100) to scale 75 - 100. set speed to 0 if scale is 0
+            if speed != 0:
+                speed = ((speed / 100) * 30) + 70;
 
             pwm_left.start(speed)
             pwm_right.start(speed)
 
             GPIO.output(MOTOR_LEFT_FORWARD_PIN, GPIO.HIGH)
             GPIO.output(MOTOR_LEFT_BACKWARD_PIN, GPIO.LOW)
+            GPIO.output(MOTOR_RIGHT_FORWARD_PIN, GPIO.HIGH)
+            GPIO.output(MOTOR_RIGHT_BACKWARD_PIN, GPIO.LOW)
+
+            time.sleep(secs)
+
+            pwm_left.stop()
+            pwm_right.stop()
+
+        elif int(inp) == 5:
+            print("How many seconds?")
+            secs = float(input())
+            print("Speed %? (0 - 100)")
+            speed = float(input())
+
+            # convert speed (Scale 0 - 100) to scale 75 - 100. set speed to 0 if scale is 0
+            if speed != 0:
+                speed = ((speed / 100) * 30) + 70;
+
+            pwm_left.start(speed)
+            pwm_right.start(speed)
+
+            GPIO.output(MOTOR_LEFT_FORWARD_PIN, GPIO.LOW)
+            GPIO.output(MOTOR_LEFT_BACKWARD_PIN, GPIO.HIGH)
             GPIO.output(MOTOR_RIGHT_FORWARD_PIN, GPIO.LOW)
             GPIO.output(MOTOR_RIGHT_BACKWARD_PIN, GPIO.HIGH)
 
-            time.sleep(3)
+            time.sleep(secs)
 
             pwm_left.stop()
             pwm_right.stop()
